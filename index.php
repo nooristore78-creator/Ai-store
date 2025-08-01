73cdf61a4e264ad970f7ecde15d116623f36a51a
nooristore78-creator <nooristore78@gmail.com>
2025-08-01 15:12:55 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,389 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>AI تکنالوژی</title>
+  <link rel="stylesheet" href="">
+  <style>
+    
+
+header {
+  background-color: transparent;
+
+}
+
+.content-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.content-container h1 {
+  font-size: 4rem;
+  color: #fff;
+}
+
+.content-container p {
+  width: 600px;
+  text-align: center;
+  margin-bottom: 40px;
+  color: #fff;
+  line-height: 24px;
+}
+
+.cta-btn {
+  padding: 12px 30px;
+  font-size: 1.2rem;
+  outline: none;
+  border: none;
+  background-color: crimson;
+  color: #fff;
+  cursor: pointer;
+}
+
+.modal-container {
+  background-color: transparent;
+  display: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+ 
+}
+
+.modal {
+  background: #fff;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  position: absolute;
+  overflow: scroll;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 100%;
+  width: 98%;
+  margin: auto;
+  
+}
+/**/
+/**/
+
+/**/
+/**/
+/**/
+
+
+
+
+
+
+
+  </style>
+
+</head>
+
+<body>
+
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="login/login.php">
+            <h1><span>A</span>I<span>ت</span>کنالوژی</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#hero" data-after="صفحه">صفحه اصلی </a></li>
+            <li><a href="#services" data-after="خدمات">خدمات ما</a></li>
+            <li><a href="#projects" data-after="محصولات">محصولات ما</a></li>
+            <li><a href="#about" data-after="درباره">درباره ما</a></li>
+            <li><a href="#contact" data-after="تماس">تماس باما</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  <!-- Hero Section  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>به وبسایت AI تکنالوژی <span></span></h1>
+        <h1>خوش آمدید<span></span></h1>
+        <h1>انشاالله که از خدمات ما راضی باشید<span></span></h1>
+        <a href="#projects" type="button" class="cta">دیدن بیشتر</a>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section  -->
+
+  <!-- Service Section -->
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">خدم<span>ات </span>ما</h1>
+        <p>ما در این مکان جمع شده ایم که خدمات بی نزیر را به شما ارایه دهیم خدمات ما از قبیل زیر میباشد!</p>
+      </div>
+      <div class="service-bottom">
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>فروش آنواع محصولات</h2>
+          <p>ما در این مکان فروش انواع محصولات را به شما پیشکش می نمایم و از محصولات گوناگون ما لذت ببرید</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>پشتیبانی پس از فروش</h2>
+          <p>تمام محصولات ما در صورت مشکل وعدم رضایت مشتری ما از محصول خود پشتیبانی میکنیم</p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>خدمات سرتاسری</h2>
+          <p>خدمات ما در سرتاسر افغانستان و حتی در کشور های دیگر نیز خدمات ارسال داریم </p>
+        </div>
+        <div class="service-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png" /></div>
+          <h2>پاسخ دهی سریع</h2>
+          <p>تیم ما افتخار دارد که خدمات درایوری منظم داشته ودر زود ترین فرصد اجناس تان را تحویل میدهد</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Service Section -->
+
+  <!-- Projects Section -->
+  <header>
+    <div class="content-container"> <!-- Projects Section -->
+      <section id="projects">
+        <div class="projects container">
+          <div class="projects-header">
+            <h1 class="section-title" style="font-size: 30px; align-items: center; color: black; font-weight: 300;">محصولات <span style="color: crimson;">ما</span></h1>
+            <p style="color: black; font-weight: 300; font-size: 1.5rem;">مابرای شمابهترین اجناس را از مشهورترین کمپنی های جهان برای شما آماده کرده ایم برای دانستن بیشتر ودیدن محصول ما لطفا بالای دکمه زیر کلیک کنید</p>
+          </div>
+          <a href="pages/index.php" class="btn1">دیدن بیشتر</a>
+          
+            </div>
+          </div>
+          
+        </div>
+      </section>
+      <!-- End Projects Section -->
+    
+    
+    </div>
+  </header>
+
+
+           
+          </div>
+        </section>
+      </section>
+     
+
+  
+  <!-- End Projects Section -->
+
+  <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+      <div class="col-left" dir="rtl">
+        <div class="about-img">
+          <img src="./img/img-2.png" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title" style="margin-bottom: 20px;">درباره  <span>ما</span></h1>
+        <h2 style="text-align: right;">درباره کارایی وبسایت</h2>
+        <p>این وبسایت توسط یک تیم کوچک  به رهبری نبی الله ایوبی  برای خدمات آنلاین وفروش محصولات در سراسر افغانستان و جهان طراحی و ساخته شده است که
+          باخدمات ارسال سریع و امن پشتیبانی از محصولات ورسیدگی به شکایات مشتریان میباشد
+        </p>
+        <a href="#" class="cta">دانلود معلومات </a>
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
+
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">تماس  <span>باما</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png" /></div>
+          <div class="contact-info">
+            <h1>موبایل</h1>
+            <h2>+(93) 773475232</h2>
+            <h2>+(93) 707815686</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png" /></div>
+          <div class="contact-info">
+            <h1>ایمیل </h1>
+            <h2>nabiullah2020@gmail.com</h2>
+            <h2>ziurehaman2020@gmail.com</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png" /></div>
+          <div class="contact-info">
+            <h1>آدرس مرکزی</h1>
+            <h2>شهر جدید, کابل ,افغانستان</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1><span>A</span>I<span>ت</span>کنالوژی</h1>
+      </div>
+      <h2>وبسایت مورد علاقه شما</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/twitter.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/behance.png" /></a>
+        </div>
+      </div>
+      <p>Copyright © 2025 noori. All rights reserved</p>
+    </div>
+  </section>
+
+
+
+ 
+  <!-- End Footer -->
+  <script src="./app.js"></script>
+  <script>
+    const canvas = document.getElementById("canvasID");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const particlesArr = [];
+let hue = 0;
+let frame = 0;
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+canvas.addEventListener("click", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  hue += 8;
+  if (particlesArr.length < 100) {
+    for (let i = 0; i < 50; i++) {
+      particlesArr.push(new Particle());
+    }
+  }
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  hue += 2;
+  if (frame % 2 === 0) {
+    for (let i = 0; i < 7; i++) {
+      particlesArr.push(new Particle());
+    }
+  }
+});
+
+class Particle {
+  constructor() {
+    this.x = mouse.x;
+    this.y = mouse.y;
+    this.size = Math.random() * 15 + 1;
+    this.speedX = Math.random() * 5 - 1.5;
+    this.speedY = Math.random() * 5 - 1.5;
+    this.color = "hsl(" + hue + ",100%,50%)";
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.1;
+  }
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function managePar() {
+  for (let i = 0; i < particlesArr.length; i++) {
+    for (let j = i; j < particlesArr.length; j++) {
+      const dx = particlesArr[i].x - particlesArr[j].x;
+      const dy = particlesArr[i].y - particlesArr[j].y;
+      const dis = Math.sqrt(dx * dx + dy * dy);
+
+      if (dis < 100) {
+        ctx.beginPath();
+        ctx.strokeStyle = particlesArr[i].color;
+        ctx.lineWidth = 0.2;
+        ctx.moveTo(particlesArr[i].x, particlesArr[i].y);
+        ctx.lineTo(particlesArr[j].x, particlesArr[j].y);
+        ctx.stroke();
+        ctx.closePath();
+      }
+    }
+    particlesArr[i].update();
+    particlesArr[i].draw();
+
+    if (particlesArr[i].size <= 0.3) {
+      particlesArr.splice(i, 1);
+      console.log(particlesArr.length);
+      i--;
+    }
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  managePar();
+  frame++;
+  requestAnimationFrame(animate);
+}
+animate();
+
+  </script>
+ 
+
+</body>
+
+</html>
